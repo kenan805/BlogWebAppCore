e510b28bcd56b9292f0e7066af04b76cefc65d6e
Kənan İdayatov <idayatovkenan7@gmail.com>
2022-03-20 23:40:06 +0400

    Listing the categories, blog project tema, new partial views and listing the blogs added

---
+++ CoreDemo/Controllers/BlogController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class BlogController : Controller
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogRepository());
+        public IActionResult Index()
+        {
+            var values = blogManager.GetAll();
+            return View(values);
+        }
+    }
+}
