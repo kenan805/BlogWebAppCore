c94ced1ac2b0dd9b0f9c8fd6c1cd8861bcaed873
Kənan İdayatov <idayatovkenan7@gmail.com>
2022-04-02 03:04:08 +0400

    Writer dashboard began to be added(1)

---
+++ CoreDemo/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
