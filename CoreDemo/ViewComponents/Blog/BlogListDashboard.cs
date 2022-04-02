6c52f0732d9bd06ad76dfa991b06757a86d5737e
Kənan İdayatov <idayatovkenan7@gmail.com>
2022-04-03 01:03:28 +0400

    BlogListDashboard and CategoryListDashboard view components added

---
+++ CoreDemo/ViewComponents/Blog/BlogListDashboard.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Blog
+{
+    public class BlogListDashboard : ViewComponent
+    {
+        BlogManager _blogManager = new BlogManager(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _blogManager.GetBlogListWithCategory();
+            return View(values);
+        }
+    }
+}
---
+++ CoreDemo/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Category
+{
+    public class CategoryListDashboard : ViewComponent
+    {
+        CategoryManager _categoryManager = new CategoryManager(new EfCategoryRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = _categoryManager.GetAll();
+            return View(values);
+        }
+    }
+}
