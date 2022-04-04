1d390b35423644d1334cf78c0edea7bb202fa577
Kənan İdayatov <idayatovkenan7@gmail.com>
2022-04-05 00:36:19 +0400

    Added admin tema and admin layout

---
+++ CoreDemo/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    [AllowAnonymous]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
