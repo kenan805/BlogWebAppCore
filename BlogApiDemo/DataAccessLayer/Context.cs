2408b9c45682577c4802066371df53c6a8e51329
Kənan İdayatov <idayatovkenan7@gmail.com>
2022-07-15 16:55:21 +0400

    WebApi intro

---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=WIN-ML3N7I995PP;database=CoreBlogApiDb; integrated security=true;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
---
+++ BlogApiDemo/DataAccessLayer/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Employee
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
