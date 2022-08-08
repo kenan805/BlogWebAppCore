0562884b44330cd341b0be44bd457040c2464e40
Kənan İdayatov <idayatovkenan7@gmail.com>
2022-08-08 21:43:40 +0400

    jwt confguration and test token created

---
+++ Jwt_core_kamp/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using Jwt_core_kamp.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jwt_core_kamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult Login()
+        {
+            return Created("", new BuildToken().CreateToken());
+        }
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public IActionResult Page1()
+		{
+            return Ok("Sayfa 1 Icin Girisini basarili");
+		}
+    }
+}
---
+++ Jwt_core_kamp/DAL/BuildToken.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Jwt_core_kamp.DAL
+{
+    public class BuildToken
+    {
+        public string CreateToken()
+        {
+            var bytes = Encoding.UTF8.GetBytes("aspnetcoreprojectcamp");
+            SymmetricSecurityKey key = new(bytes);
+            SigningCredentials credentials = new(key, SecurityAlgorithms.HmacSha256);
+            JwtSecurityToken token = new(issuer: "http://localhost", audience: "http://localhost", notBefore: DateTime.Now, expires: DateTime.Now.AddMinutes(1), signingCredentials: credentials);
+            JwtSecurityTokenHandler handler = new();
+            return handler.WriteToken(token);
+        }
+    }
+}
